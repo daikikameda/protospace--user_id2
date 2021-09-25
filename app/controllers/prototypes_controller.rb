5b261ca46e9a88201105708ad263b32d5a910c69
daikikameda <daiki.d.0124@gmail.com>
2021-09-25 22:17:45 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,63 @@
+class PrototypesController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_action :authenticate_user!
+  before_action :move_to_index, except: [:index, :show]
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototypes = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:prototype_id] || params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    if user_signed_in?
+      redirect_to action: :index
+    else 
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to action: "show"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+end
